aec574122a5281f4b30e61eafe1a35997e7f19d2
Martijn <martijn@tidalcontrol.com>
2024-11-13 12:21:33 +0100

    chore: fix

--- connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ServicePlanName.java
+++ connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ServicePlanName.java
@@ -1,47 +1,51 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
 package com.blazebit.query.connector.azure.graph;
 
 import java.util.Optional;
 
 public enum ServicePlanName {
-    AAD_PREMIUM("AAD_PREMIUM"),
-    AAD_PREMIUM_P2("AAD_PREMIUM_P2"),
-    INTUNE_A("INTUNE_A"),
-    INTUNE_A_GOV("INTUNE_A_GOV"),
-    INTUNE_A_VL("INTUNE_A_VL"),
-    INTUNE_O365("INTUNE_O365"),
-    INTUNE_Defender("Intune_Defender"),
-    INTUNE_AdvancedEA("Intune_AdvancedEA"),
-    INTUNE_EPM("Intune-EPM"),
-    INTUNE_MAMTunnel("Intune-MAMTunnel"),
-    INTUNE_EDU("INTUNE_EDU"),
-    INTUNE_P2("INTUNE_P2"),
-    INTUNE_SMBIZ("INTUNE_SMBIZ");
+	AAD_PREMIUM("AAD_PREMIUM"),
+	AAD_PREMIUM_P2("AAD_PREMIUM_P2"),
+	INTUNE_A("INTUNE_A"),
+	INTUNE_A_GOV("INTUNE_A_GOV"),
+	INTUNE_A_VL("INTUNE_A_VL"),
+	INTUNE_O365("INTUNE_O365"),
+	INTUNE_Defender("Intune_Defender"),
+	INTUNE_AdvancedEA("Intune_AdvancedEA"),
+	INTUNE_EPM("Intune-EPM"),
+	INTUNE_MAMTunnel("Intune-MAMTunnel"),
+	INTUNE_EDU("INTUNE_EDU"),
+	INTUNE_P2("INTUNE_P2"),
+	INTUNE_SMBIZ("INTUNE_SMBIZ");
 
-    private final String name;
+	private final String name;
 
-    ServicePlanName(String name) {
-        this.name = name;
-    }
+	ServicePlanName(String name) {
+		this.name = name;
+	}
 
-    @Override
-    public String toString() {
-        return this.name;
-    }
+	@Override
+	public String toString() {
+		return this.name;
+	}
 
-    public static Optional<ServicePlanName> fromName(String name) {
-        for (ServicePlanName plan : values()) {
-            if (plan.name.equals(name)) {
-                return Optional.of(plan);
-            }
-        }
-        return Optional.empty();
-    }
+	public static Optional<ServicePlanName> fromName(String name) {
+		for (ServicePlanName plan : values()) {
+			if (plan.name.equals(name)) {
+				return Optional.of(plan);
+			}
+		}
+		return Optional.empty();
+	}
 
-    public boolean isAad() {
-        return this.name.toUpperCase().startsWith("AAD");
-    }
+	public boolean isAad() {
+		return this.name.toUpperCase().startsWith("AAD");
+	}
 
-    public boolean isIntune() {
-        return this.name.toUpperCase().startsWith("INTUNE");
-    }
+	public boolean isIntune() {
+		return this.name.toUpperCase().startsWith("INTUNE");
+	}
 }
--- connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ServicePlanUtils.java
+++ connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ServicePlanUtils.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
 package com.blazebit.query.connector.azure.graph;
 
 import com.microsoft.graph.beta.models.ServicePlanInfo;
@@ -8,21 +12,21 @@ import java.util.List;
 import java.util.Objects;
 
 public class ServicePlanUtils {
-    /**
-     * Extracts all service plan names from a list of SubscribedSku objects.
-     *
-     * @param subscribedSkus the list of SubscribedSku
-     * @return a list of all service plan names across all SubscribedSku
-     */
-    public static List<ServicePlanName> getAllServicePlanNames(List<SubscribedSku> subscribedSkus) {
-        List<ServicePlanName> servicePlanNames = new ArrayList<>();
+	/**
+	 * Extracts all service plan names from a list of SubscribedSku objects.
+	 *
+	 * @param subscribedSkus the list of SubscribedSku
+	 * @return a list of all service plan names across all SubscribedSku
+	 */
+	public static List<ServicePlanName> getAllServicePlanNames(List<SubscribedSku> subscribedSkus) {
+		List<ServicePlanName> servicePlanNames = new ArrayList<>();
 
-        for (SubscribedSku sku : subscribedSkus) {
-            for (ServicePlanInfo servicePlan : Objects.requireNonNull(sku.getServicePlans())) {
-                ServicePlanName.fromName(servicePlan.getServicePlanName()).ifPresent(servicePlanNames::add);
-            }
-        }
+		for (SubscribedSku sku : subscribedSkus) {
+			for (ServicePlanInfo servicePlan : Objects.requireNonNull(sku.getServicePlans())) {
+				ServicePlanName.fromName(servicePlan.getServicePlanName()).ifPresent(servicePlanNames::add);
+			}
+		}
 
-        return servicePlanNames;
-    }
+		return servicePlanNames;
+	}
 }
