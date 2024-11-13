de0295c0c55a41eff69f742787f31c4e72d2594c
Martijn <martijn@tidalcontrol.com>
2024-11-13 12:19:52 +0100

    feat: add organizations

--- connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ManagedDeviceDataFetcher.java
+++ connector/azure/graph/src/main/java/com/blazebit/query/connector/azure/graph/ManagedDeviceDataFetcher.java
@@ -10,14 +10,11 @@ import com.blazebit.query.spi.DataFetcher;
 import com.blazebit.query.spi.DataFetcherException;
 import com.blazebit.query.spi.DataFormat;
 import com.microsoft.graph.beta.models.ManagedDevice;
-import com.microsoft.graph.beta.models.SubscribedSku;
 import com.microsoft.graph.beta.serviceclient.GraphServiceClient;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.blazebit.query.connector.azure.graph.ServicePlanUtils.getAllServicePlanNames;
 
 /**
  * @author Max Hovens
@@ -30,28 +27,24 @@ public class ManagedDeviceDataFetcher implements DataFetcher<ManagedDevice>, Ser
 	private ManagedDeviceDataFetcher() {
 	}
 
-    /**
-     * @param context The data fetching context
-     * @return A list of ManagedDevices
-     * @throws com.microsoft.graph.beta.models.odataerrors.ODataError with message "AADSTS500014: The service principal for resource '0000000a-0000-0000-c000-000000000000' is disabled. etc....`" when Microsoft Intune is not activated on the tenant
-     */
-    @Override
-    public List<ManagedDevice> fetch(DataFetchContext context) {
-        try {
-            List<GraphServiceClient> graphClients = AzureGraphConnectorConfig.GRAPH_SERVICE_CLIENT.getAll(context);
-            List<ManagedDevice> list = new ArrayList<>();
-            for (GraphServiceClient graphClient : graphClients) {
-                List<SubscribedSku> subscribedSkus = (List<SubscribedSku>) context.getSession().getOrFetch(SubscribedSku.class);
-                List<ServicePlanName> servicePlanNames = getAllServicePlanNames(subscribedSkus);
-                if(servicePlanNames.stream().anyMatch(ServicePlanName::isIntune)) {
-                    list.addAll(graphClient.deviceManagement().managedDevices().get().getValue());
-                }
-            }
-            return list;
-        } catch (RuntimeException e) {
-            throw new DataFetcherException("Could not fetch managed device list", e);
-        }
-    }
+	/**
+	 * @param context The data fetching context
+	 * @return A list of ManagedDevices
+	 * @throws com.microsoft.graph.beta.models.odataerrors.ODataError with message "AADSTS500014: The service principal for resource '0000000a-0000-0000-c000-000000000000' is disabled. etc....`" when Microsoft Intune is not activated on the tenant
+	 */
+	@Override
+	public List<ManagedDevice> fetch(DataFetchContext context) {
+		try {
+			List<GraphServiceClient> graphClients = AzureGraphConnectorConfig.GRAPH_SERVICE_CLIENT.getAll(context);
+			List<ManagedDevice> list = new ArrayList<>();
+			for (GraphServiceClient graphClient : graphClients) {
+				list.addAll(graphClient.deviceManagement().managedDevices().get().getValue());
+			}
+			return list;
+		} catch (RuntimeException e) {
+			throw new DataFetcherException("Could not fetch managed device list", e);
+		}
+	}
 
 	@Override
 	public DataFormat getDataFormat() {
