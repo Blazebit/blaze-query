e3ed5cb8961570931f93fc2db19d8a81bf2b5832
Martijn <martijn@tidalcontrol.com>
2024-11-13 12:32:11 +0100

    remove ARM tests and objects

--- connector/azure/graph/src/test/java/com/blazebit/query/connector/azure/graph/AzureTestObjects.java
+++ connector/azure/graph/src/test/java/com/blazebit/query/connector/azure/graph/AzureTestObjects.java
@@ -4,74 +4,15 @@
  */
 package com.blazebit.query.connector.azure.graph;
 
-import com.azure.json.JsonOptions;
-import com.azure.json.JsonReader;
-import com.azure.json.implementation.DefaultJsonReader;
-import com.azure.resourcemanager.compute.fluent.models.VirtualMachineInner;
-import com.azure.resourcemanager.keyvault.fluent.models.VaultInner;
-import com.azure.resourcemanager.storage.fluent.models.BlobServicePropertiesInner;
-import com.azure.resourcemanager.storage.fluent.models.StorageAccountInner;
-import com.microsoft.graph.beta.models.ConditionalAccessPolicy;
 import com.microsoft.graph.beta.models.ManagedDevice;
 import com.microsoft.graph.beta.models.Organization;
 import com.microsoft.graph.beta.models.User;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public final class AzureTestObjects {
 
 	private AzureTestObjects() {}
-
-	public static VirtualMachineInner linuxVMWithPasswordEnabled() {
-			return TestObject.fromJson( VirtualMachineInner.class,
-					new File(
-							"src/test/resources/resource-definitions/azure/virtual-machine/linux-vm-with-password-enabled.json"));
-
-	}
-
-	public static VirtualMachineInner linuxVMWithoutPasswordEnabled() {
-			return TestObject.fromJson(VirtualMachineInner.class,
-					new File(
-							"src/test/resources/resource-definitions/azure/virtual-machine/linux-vm-without-password-enabled.json"));
-	}
-
-	public static ConditionalAccessPolicy globalAdministratorsMFAConditionalAccessPolicy() {
-		return TestObject.fromJson(
-				ConditionalAccessPolicy.class,
-				new File(
-						"src/test/resources/resource-definitions/azure/conditional-access-policy/global-administrators-mfa.json"));
-	}
-
-	public static StorageAccountInner storageAccount() {
-			return TestObject.fromJson(StorageAccountInner.class,
-					new File(
-							"src/test/resources/resource-definitions/azure/storage-account/storage-account.json"));
-	}
-
-	public static BlobServicePropertiesInner blobServiceProperties() {
-			return TestObject.fromJson(BlobServicePropertiesInner.class,
-					new File(
-							"src/test/resources/resource-definitions/azure/storage-account/blob-service-properties.json"));
-
-	}
-
-	public static VaultInner keyVault() {
-			return TestObject.fromJson(VaultInner.class,
-					new File("src/test/resources/resource-definitions/azure/key-vault/key-vault.json"));
-	}
-
-	private static JsonReader jsonReader(String file) throws IOException {
-		return DefaultJsonReader.fromStream(new FileInputStream(file), new JsonOptions());
-	}
-
-	public static VaultInner keyVaultWithDefaultNetworkAcls() {
-		return TestObject.fromJson(
-				VaultInner.class,
-				new File(
-						"src/test/resources/resource-definitions/azure/key-vault/key-vault-default-network-acls.json"));
-	}
 
 	// https://graph.microsoft.com/beta/deviceManagement/managedDevices?$select=deviceName,isEncrypted,emailAddress,userPrincipalName,model,manufacturer,managementAgent
 	public static ManagedDevice managedDeviceUnencrypted() {
