7f17376d6b0ca5cbc7464a27efa3e1a8e212caab
Max Hovens <14804474+maxhov@users.noreply.github.com>
2024-12-07 11:48:04 +0100

    Remove unused connectors (#31)
    Azure connectors based on OpenAPI generator are no longer used and
    superseeded by Resource Manager

--- connector/azure/blob-services/v20230501/jersey3/src/main/java/com/blazebit/query/connector/azure/blob/services/v20230501/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright Blazebit
- */
-
-/**
- * Connector for the Azure Blob Services Endpoint version 2023-05-01.
- */
-package com.blazebit.query.connector.azure.blob.services.v20230501;
