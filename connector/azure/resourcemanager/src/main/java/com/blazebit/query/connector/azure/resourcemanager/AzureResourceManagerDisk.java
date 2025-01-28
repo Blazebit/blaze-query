52f5a325f90eadae5ff901acf91ea845f2802983
Martijn <84505880+mjsprengers@users.noreply.github.com>
2025-01-28 15:32:12 +0100

    feat: add data fetcher for Azure disks

---
+++ connector/azure/resourcemanager/src/main/java/com/blazebit/query/connector/azure/resourcemanager/AzureResourceManagerDisk.java
@@ -0,0 +1,18 @@
+package com.blazebit.query.connector.azure.resourcemanager;
+
+import com.azure.resourcemanager.compute.fluent.models.DiskInner;
+
+/**
+ * @author Martijn Sprengers
+ * @since 1.0.2
+ */
+public class AzureResourceManagerDisk extends AzureResourceManagerWrapper<DiskInner> {
+	public AzureResourceManagerDisk(String tenantId, String resourceId, DiskInner payload) {
+		super(tenantId, resourceId, payload);
+	}
+
+	@Override
+	public DiskInner getPayload() {
+		return super.getPayload();
+	}
+}
