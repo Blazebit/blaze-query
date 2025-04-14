4dac097931b504ef5500f694c8ec57623fc68f8a
Dimitar Prisadnikov <dimitar@tidalcontrol.com>
2025-04-14 15:53:52 +0200

    add tests and test objects

---
+++ connector/github-graphql/src/test/java/GitHubGraphQlDataFetcherTest.java
@@ -0,0 +1,56 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
+import com.blazebit.query.QueryContext;
+import com.blazebit.query.TypeReference;
+import com.blazebit.query.connector.github.graphql.GitHubOrganization;
+import com.blazebit.query.connector.github.graphql.GitHubRepository;
+import com.blazebit.query.connector.github.graphql.GitHubSchemaProvider;
+import com.blazebit.query.impl.QueryContextBuilderImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+
+public class GitHubGraphQlDataFetcherTest {
+	private static final QueryContext CONTEXT;
+
+	static {
+		var builder = new QueryContextBuilderImpl();
+		builder.registerSchemaProvider( new GitHubSchemaProvider() );
+		builder.registerSchemaObjectAlias( GitHubRepository.class, "GitHubRepository" );
+		builder.registerSchemaObjectAlias( GitHubOrganization.class, "GitHubOrganization" );
+		CONTEXT = builder.build();
+	}
+
+	@Test
+	void should_return_github_repository() {
+		try (var session = CONTEXT.createSession()) {
+			GitHubRepository repository = GitHubGraphQlTestObjects.repository();
+			session.put(
+					GitHubRepository.class, List.of(repository));
+
+			var typedQuery =
+					session.createQuery( "select r.* from GitHubRepository r", new TypeReference<Map<String, Object>>() {});
+
+			assertThat(typedQuery.getResultList()).extracting(result -> result.get("id")).containsExactly( repository.id());
+		}
+	}
+
+	@Test
+	void should_return_github_organization() {
+		try (var session = CONTEXT.createSession()) {
+			GitHubOrganization organization = GitHubGraphQlTestObjects.organization();
+			session.put(
+					GitHubOrganization.class, List.of(organization));
+
+			var typedQuery =
+					session.createQuery( "select o.* from GitHubOrganization o", new TypeReference<Map<String, Object>>() {});
+
+			assertThat(typedQuery.getResultList()).extracting(result -> result.get("id")).containsExactly( organization.id());
+		}
+	}
+}
---
+++ connector/github-graphql/src/test/java/GitHubGraphQlTestObjects.java
@@ -0,0 +1,60 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright Blazebit
+ */
+import com.blazebit.query.connector.github.graphql.GitHubBranchProtectionRule;
+import com.blazebit.query.connector.github.graphql.GitHubOrganization;
+import com.blazebit.query.connector.github.graphql.GitHubRepository;
+import com.blazebit.query.connector.github.graphql.GitHubRule;
+import com.blazebit.query.connector.github.graphql.GitHubRuleset;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+public class GitHubGraphQlTestObjects {
+	private GitHubGraphQlTestObjects() {
+	}
+
+	public static GitHubRepository repository() {
+		return new GitHubRepository(
+				"123",
+				"My Repository",
+				"Description of my repository",
+				false,
+				false,
+				true,
+				true,
+				false,
+				GitHubRepository.Visibility.INTERNAL,
+				OffsetDateTime.of(2025, 1, 1, 9, 30, 0, 0, ZoneOffset.UTC),
+				new GitHubRepository.DefaultBranch("REF_someId123", "main"),
+				rulesets(),
+				branchProtectionRules());
+	}
+
+	public static List<GitHubRuleset> rulesets() {
+		GitHubRule.RuleParameters prParams = new GitHubRule.RuleParameters(true, 0);
+		GitHubRule pullRequestRule = new GitHubRule("PULL_REQUEST", prParams);
+
+		GitHubRuleset.RulesetConditions conditions1 = new GitHubRuleset.RulesetConditions(List.of());
+		GitHubRuleset ruleset =
+				new GitHubRuleset("BRANCH", "ACTIVE", conditions1, List.of(pullRequestRule));
+
+		return List.of(ruleset);
+	}
+
+	public static List<GitHubBranchProtectionRule> branchProtectionRules() {
+		GitHubBranchProtectionRule.MatchingRef matchingRef =
+				new GitHubBranchProtectionRule.MatchingRef("REF_someId123", "main");
+		GitHubBranchProtectionRule rule =
+				new GitHubBranchProtectionRule("BPR_anotherId321", false, true, List.of(matchingRef));
+
+		return List.of(rule);
+	}
+
+	public static GitHubOrganization organization() {
+		return new GitHubOrganization("org_123", "My Organization", true);
+	}
+
+}
