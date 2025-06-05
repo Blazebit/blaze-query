6192fa770d94992d342c0d058d504c8baebab3d2
Dimitar Prisadnikov <dimitar@tidalcontrol.com>
2025-06-05 11:05:58 +0200

    switch from the deprecated `getAllProjects()` to `searchProjectsWithHttpInfo()` for fetching Jira Cloud projects

--- connector/jira/cloud/jersey3/src/main/java/com/blazebit/query/connector/jira/cloud/ProjectDataFetcher.java
+++ connector/jira/cloud/jersey3/src/main/java/com/blazebit/query/connector/jira/cloud/ProjectDataFetcher.java
@@ -8,6 +8,8 @@ import com.blazebit.query.connector.base.DataFormats;
 import com.blazebit.query.connector.jira.cloud.api.ProjectsApi;
 import com.blazebit.query.connector.jira.cloud.invoker.ApiClient;
 import com.blazebit.query.connector.jira.cloud.invoker.ApiException;
+import com.blazebit.query.connector.jira.cloud.invoker.ApiResponse;
+import com.blazebit.query.connector.jira.cloud.model.PageBeanProject;
 import com.blazebit.query.connector.jira.cloud.model.Project;
 import com.blazebit.query.spi.DataFetchContext;
 import com.blazebit.query.spi.DataFetcher;
@@ -17,6 +19,7 @@ import com.blazebit.query.spi.DataFormat;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Christian Beikov
@@ -36,14 +39,53 @@ public class ProjectDataFetcher implements DataFetcher<Project>, Serializable {
 			List<Project> list = new ArrayList<>();
 			for ( ApiClient apiClient : apiClients ) {
 				ProjectsApi api = new ProjectsApi( apiClient );
-				list.addAll( api.getAllProjects( null, null, null ) );
+				list.addAll( fetchAllProjectsWithPagination(api) );
 			}
 			return list;
-		}
-		catch (ApiException e) {
+		} catch (ApiException e) {
 			throw new DataFetcherException( "Could not fetch project list", e );
 		}
 	}
+
+
+	private List<Project> fetchAllProjectsWithPagination(ProjectsApi api) throws ApiException {
+		List<Project> allProjects = new ArrayList<>();
+		Long startAt = 0L;
+		boolean hasMoreResults = true;
+
+		while (hasMoreResults) {
+			ApiResponse<PageBeanProject> response = api.searchProjectsWithHttpInfo(
+					startAt,
+					null, // 50 by default
+					null,
+					null,
+					null,
+					null,
+					null,
+					null,
+					null,
+					null,
+					List.of("live", "archived"),
+					null,
+					null
+			);
+
+			PageBeanProject page = response.getData();
+			if (page.getValues() != null) {
+				allProjects.addAll(page.getValues());
+			}
+
+			Long total = page.getTotal();
+			if (total == null || startAt + Objects.requireNonNull( page.getValues() ).size() >= total) {
+				hasMoreResults = false;
+			} else {
+				startAt += page.getMaxResults();
+			}
+		}
+
+		return allProjects;
+	}
+
 
 	@Override
 	public DataFormat getDataFormat() {
