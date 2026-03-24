ea8b146f9e6130498247a332d5ab3474b53c2af0
maxhov <14804474+maxhov@users.noreply.github.com>
2026-03-24 20:58:38 +0100

    Revert try-with-resources on NetHttpTransport (not AutoCloseable)

--- connector/google/gcp/sql/src/main/java/com/blazebit/query/connector/gcp/sql/SqlInstanceDataFetcher.java
+++ connector/google/gcp/sql/src/main/java/com/blazebit/query/connector/gcp/sql/SqlInstanceDataFetcher.java
@@ -13,7 +13,6 @@ import com.blazebit.query.spi.DataFetcherException;
 import com.blazebit.query.spi.DataFormat;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.services.sqladmin.SQLAdmin;
@@ -55,41 +54,39 @@ public class SqlInstanceDataFetcher implements DataFetcher<GcpSqlInstance>, Seri
 			List<? extends GcpProject> projects = context.getSession().getOrFetch( GcpProject.class );
 			for ( CredentialsProvider credentialsProvider : credentialsProviders ) {
 				GoogleCredentials credentials = (GoogleCredentials) credentialsProvider.getCredentials();
-				try (NetHttpTransport transport = GoogleNetHttpTransport.newTrustedTransport()) {
-					SQLAdmin sqladmin = new SQLAdmin.Builder(
-							transport,
-							GsonFactory.getDefaultInstance(),
-							new HttpCredentialsAdapter( credentials ) )
-							.setApplicationName( APPLICATION_NAME )
-							.build();
-					for ( GcpProject project : projects ) {
-						try {
-							String projectId = project.getPayload().getProjectId();
-							SQLAdmin.Instances.List request = sqladmin.instances().list( projectId );
-							InstancesListResponse response;
-							do {
-								response = request.execute();
-								if ( response.getItems() != null ) {
-									for ( DatabaseInstance instance : response.getItems() ) {
-										list.add( new GcpSqlInstance( instance.getName(), instance ) );
-									}
+				SQLAdmin sqladmin = new SQLAdmin.Builder(
+						GoogleNetHttpTransport.newTrustedTransport(),
+						GsonFactory.getDefaultInstance(),
+						new HttpCredentialsAdapter( credentials ) )
+						.setApplicationName( APPLICATION_NAME )
+						.build();
+				for ( GcpProject project : projects ) {
+					try {
+						String projectId = project.getPayload().getProjectId();
+						SQLAdmin.Instances.List request = sqladmin.instances().list( projectId );
+						InstancesListResponse response;
+						do {
+							response = request.execute();
+							if ( response.getItems() != null ) {
+								for ( DatabaseInstance instance : response.getItems() ) {
+									list.add( new GcpSqlInstance( instance.getName(), instance ) );
 								}
-								request.setPageToken( response.getNextPageToken() );
 							}
-							while ( response.getNextPageToken() != null );
+							request.setPageToken( response.getNextPageToken() );
 						}
-						catch (GoogleJsonResponseException e) {
-							if ( e.getStatusCode() == 403 && e.getDetails() != null
-									&& e.getDetails().getErrors() != null
-									&& e.getDetails().getErrors().stream()
-									.anyMatch( err -> "accessNotConfigured".equals( err.getReason() ) ) ) {
-								LOG.log( Level.WARNING,
-										"Cloud SQL Admin API is not enabled for project ''{0}'', skipping SQL instance fetch.",
-										project.getPayload().getProjectId() );
-								continue;
-							}
-							throw e;
+						while ( response.getNextPageToken() != null );
+					}
+					catch (GoogleJsonResponseException e) {
+						if ( e.getStatusCode() == 403 && e.getDetails() != null
+								&& e.getDetails().getErrors() != null
+								&& e.getDetails().getErrors().stream()
+								.anyMatch( err -> "accessNotConfigured".equals( err.getReason() ) ) ) {
+							LOG.log( Level.WARNING,
+									"Cloud SQL Admin API is not enabled for project ''{0}'', skipping SQL instance fetch.",
+									project.getPayload().getProjectId() );
+							continue;
 						}
+						throw e;
 					}
 				}
 			}
